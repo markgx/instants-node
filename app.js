de5f516ff76b8bd73dba17aa17290224c6aa6c12
Mark Guerra <markgx@gmail.com>
2011-09-22 22:46:44 +0000

    fixed port

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.get('/callback', function(req, res) {
   res.redirect('/');
 });
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
