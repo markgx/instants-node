be725c39564ce7dae2b1fdb193e7fd5ee1712a89
Mark Guerra <markgx@gmail.com>
2011-09-22 00:46:03 -0700

    wip

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+if (CLIENT_ID == null || CLIENT_SECRET == null) {
+  console.log('ERROR: The CLIENT_ID and CLIENT_SECRET environment variables must be set.');
+  return;
+}
+
 var express = require('express');
 var app = express.createServer();
 
+var instagram = require('instagram-node-lib');
+instagram.set('client_id', CLIENT_ID);
+instagram.set('client_secret', CLIENT_SECRET);
+instagram.set('redirect_uri', 'http://localhost:3000/callback');
+
+// config
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -24,10 +38,41 @@ app.configure('production', function() {
   app.use(express.errorHandler());
 });
 
+var isLoggedIn = function(req) {
+  return req.session.accessToken != null;
+}
+
+// routes
 app.get('/', function(req, res) {
   res.render('index', {
-    title: 'Express'
+    loggedIn: isLoggedIn(req)
   });
+});
+
+app.get('/feed', function(req, res) {
+  if (!isLoggedIn(req)) {
+    res.redirect('/login');
+  }
+
+  var images = instagram.users.self({ access_token: req.session.accessToken });
+
+  // TODO:
+
+  // res.render('feed', {
+  //   images: images
+  // });
+  // res.send(images);
+});
+
+app.get('/login', function(req, res) {
+  var authUrl = instagram.oauth.authorization_url({});
+  res.redirect(authUrl);
+});
+
+app.get('/callback', function(req, res) {
+  // TODO: validate
+  req.session.accessToken = req.query['code'];
+  res.redirect('/');
 });
 
 app.listen(3000);
